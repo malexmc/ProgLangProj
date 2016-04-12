bab67c96dcf6ec72a26adb410371575ce4e6a7e4
malexmc <malexmc@gmail.com>
2016-04-12 10:01:52 -0400

    Cleaned Code
    Replaced variable names to more readable variable names, and attempted
    to comment most functions to better explain their workflows.

--- FinalProject.rkt
+++ FinalProject.rkt
@@ -6,31 +6,33 @@
 ;; definition of structures for MUPL programs - Do NOT change
 (struct var  (string) #:transparent)  ;; a variable, e.g., (var "foo")
 (struct int  (num)    #:transparent)  ;; a constant number, e.g., (int 17)
-(struct add  (e1 e2)  #:transparent)  ;; add two expressions
-(struct ifgreater (e1 e2 e3 e4)    #:transparent) ;; if e1 > e2 then e3 else e4
-(struct fun  (nameopt formal body) #:transparent) ;; a recursive(?) 1-argument function
-(struct call (funexp actual)       #:transparent) ;; function call
+(struct add  (operand1 operand2)  #:transparent)  ;; add two expressions
+(struct ifgreater (leftSideOperand rightSideOperand trueResult falseResult)    #:transparent) ;; if e1 > e2 then e3 else e4
+(struct fun  (functionName functionArg functionBody) #:transparent) ;; a recursive(?) 1-argument function
+(struct call (functionExpression functionBinding)       #:transparent) ;; function call
 (struct mlet (var e body) #:transparent) ;; a local binding (let var = e in body) 
-(struct apair (e1 e2)     #:transparent) ;; make a new pair
-(struct fst  (e)    #:transparent) ;; get first part of a pair
-(struct snd  (e)    #:transparent) ;; get second part of a pair
+(struct apair (pairHead pairTail)     #:transparent) ;; make a new pair
+(struct fst  (currentPair)    #:transparent) ;; get first part of a pair
+(struct snd  (currentPair)    #:transparent) ;; get second part of a pair
 (struct aunit ()    #:transparent) ;; unit value -- good for ending a list
-(struct isaunit (e) #:transparent) ;; evaluate to 1 if e is unit else 0
+(struct isaunit (currentUnit) #:transparent) ;; evaluate to 1 if e is unit else 0
 
 ;; a closure is not in "source" programs; it is what functions evaluate to
 (struct closure (env fun) #:transparent)
 
 ;; Part 1 - Warm-up
 
+;;Converts a racket list to a mupl list
 (define (racketlist->mupllist racketList)
   (if (null? racketList)
       (aunit)
       (apair (car racketList) (racketlist->mupllist (cdr racketList)))))
 
+;;Converts a mupl list to a racket list
 (define (mupllist->racketlist muplList)
   (if (aunit? muplList)
       null
-      (cons (apair-e1 muplList) (mupllist->racketlist (apair-e2 muplList)))))
+      (cons (apair-pairHead muplList) (mupllist->racketlist (apair-pairTail muplList)))))
 
 
 ;; Part 2 - Implementing the language
@@ -49,66 +51,129 @@
 (define (eval-under-env e env)
   (cond [(var? e) 
          (envlookup env (var-string e))]
+         
+        ;;Adds two operands
         [(add? e) 
-         (let ([v1 (eval-under-env (add-e1 e) env)]
-               [v2 (eval-under-env (add-e2 e) env)])
-           (if (and (int? v1)
-                    (int? v2))
-               (int (+ (int-num v1) 
-                       (int-num v2)))
+        
+        ;;Evaluate the parametersunder the environment
+         (let ([racketOperand1 (eval-under-env (add-operand1 e) env)]
+               [racketOperand2 (eval-under-env (add-operand2 e) env)])
+               
+           ;;If they both are ints, add them
+           (if (and (int? racketOperand1)
+                    (int? racketOperand2))
+               (int (+ (int-num racketOperand1) 
+                       (int-num racketOperand2)))
                (error "MUPL addition applied to non-number")))]
+               
+        ;; returns the given int e
         [(int? e)
          e]
+         
+        ;;Decides if leftSideOperand is greater than rightSideOperand. If so, do trueResult, else do falseResult
         [(ifgreater? e)
-         (let ([v1 (eval-under-env (ifgreater-e1 e) env)]
-               [v2 (eval-under-env (ifgreater-e2 e) env)])
-           (if (and (int? v1)
-                    (int? v2))
-               (if (> (int-num v1)
-                      (int-num v2))
-                   (eval-under-env (ifgreater-e3 e) env)
-                   (eval-under-env (ifgreater-e4 e) env))
+        
+        ;;Evaluete left and right side operands under environment
+         (let ([racketLeftSideOperand (eval-under-env (ifgreater-leftSideOperand e) env)]
+               [racketRightSideOperand (eval-under-env (ifgreater-rightSideOperand e) env)])
+               
+           ;;If racketLeftSideOperand and racketRightSideOperand are ints, compare them. If true, evaluate trueResult, else evaluate falseResult
+           (if (and (int? racketLeftSideOperand)
+                    (int? racketRightSideOperand))
+               (if (> (int-num racketLeftSideOperand)
+                      (int-num racketRightSideOperand))
+                   (eval-under-env (ifgreater-trueResult e) env)
+                   (eval-under-env (ifgreater-falseResult e) env))
                (error "MUPL ifgreater applied to non-number")))]  
+               
+               
+        ;;Evaluate function e using the racket environment as a closure
         [(fun? e) (closure env e)]
+        
+        
+        ;;Models a function call
         [(call? e)
-         (let ([cl (eval-under-env (call-funexp e) env)])
-           (if (closure? cl)
-               (let* ([fn (closure-fun cl)]
-                      [v (eval-under-env (call-actual e) env)]
-                      [env (cons (cons (fun-formal fn) v) (closure-env cl))])
-                 (if (fun-nameopt fn)
-                   (let ([env (cons (cons (fun-nameopt fn) cl) env)])
-                     (eval-under-env (fun-body fn) env))
-                   (eval-under-env (fun-body fn) env)))
+         ;;creates the racket function closure from the mupl expression
+         (let ([currentClosure (eval-under-env (call-functionExpression e) env)])
+
+           ;;If the closure looks good, , else throw error
+           (if (closure? currentClosure)
+               (let* ([racketFunctionClosure (closure-fun currentClosure)]
+                      [functionBinding (eval-under-env (call-functionBinding e) env)]
+                      [env (cons (cons (fun-functionArg racketFunctionClosure) functionBinding) (closure-env currentClosure))])
+                 (if (fun-functionName racketFunctionClosure)
+                   (let ([env (cons (cons (fun-functionName racketFunctionClosure) currentClosure) env)])
+                     (eval-under-env (fun-functionBody racketFunctionClosure) env))
+                   (eval-under-env (fun-functionBody racketFunctionClosure) env)))
                (error "First param for call is not a closure")))]
+               
+               
+        ;;Evaluates e, then evaluates the second value in an environment which maps that e's value to variable.        
         [(mlet? e)
          (let* ([assignedValue (eval-under-env (mlet-e e) env)]
                 [variable (mlet-var e)])
            (eval-under-env (mlet-body e) (cons (cons variable assignedValue) env)))]
+           
+        ;; Checks if e is apair. Returns the paired values of racketPairHead and racketPairTail
         [(apair? e)
-         (let ([v1 (eval-under-env (apair-e1 e) env)]
-               [v2 (eval-under-env (apair-e2 e) env)])
-           (apair v1 v2))]
+
+         ;;Evaluates and racketizes pairHead and pairTail 
+         (let ([racketPairHead (eval-under-env (apair-pairHead e) env)]
+               [racketPairTail (eval-under-env (apair-pairTail e) env)])
+
+           ;;Returns the new pair of racket values
+           (apair racketPairHead racketPairTail))]
+           
+           
+       ;;Gets first element of a pair.
         [(fst? e)
-         (let ([v (eval-under-env (fst-e e) env)])
-         (if (apair? v)
-             (apair-e1 v)
+        
+        ;;set racketCurrentPair as racketized MUPL pair
+         (let ([racketCurrentPair (eval-under-env (fst-currentPair e) env)])
+         
+         ;;If racketCurrentPair is actually a pair, do fst and return head of pair. Else throw error
+         (if (apair? racketCurrentPair)
+             (apair-pairHead racketCurrentPair)
              (error "MUPL first applied to non-pair")))]
+             
+             
+        ;;Gets second element of a pair.
         [(snd? e)
-         (let ([v (eval-under-env (snd-e e) env)])
-           (if (apair? v)
-               (apair-e2 v)
+        
+        ;;set racketCurrentPair as racketized MUPL pair
+         (let ([racketCurrentPair (eval-under-env (snd-currentPair e) env)])
+         
+         ;;If racketCurrentPair is actually a pair, do fst and return tail of pair. Else throw error
+           (if (apair? racketCurrentPair)
+               (apair-pairTail racketCurrentPair)
                (error "MUPL second appied to non-pair")))]
+               
+        
+        ;;returns the given aunit
         [(aunit? e) e]
+        
+        ;;Decides if variable is a unit. Units signify the end of structures (like null terminators end c++ arrays).
         [(isaunit? e)
-         (let ([v (eval-under-env (isaunit-e e) env)])
-           (if (aunit? v)
+        
+        ;;Racketize the given unit
+         (let ([racketCurrentUnit (eval-under-env (isaunit-currentUnit e) env)])
+         
+         ;;If it's actually a unit, return 1, else return 0
+           (if (aunit? racketCurrentUnit)
                (int 1)
                (int 0)
                ))]
+               
+        ;;returns the given closure 
         [(closure? e)
          e]
+         
+         
         [#t (error "bad MUPL expression")]))
+        
+        
+        
+        
 
 ;; Do NOT change
 (define (eval-exp e)
@@ -116,31 +181,42 @@
         
 ;; Part 3 - Expanding the language
 
-(define (ifaunit e1 e2 e3)
+;;If e1 is a unit, then return trueResult, else return falseResult
+(define (ifaunit e1 trueResult falseREsult)
   (ifgreater (isaunit e1)
              (int 0)
-             e2
-             e3))
+             trueResult
+             falseREsult))
 
-(define (mlet* listlist e2)
-  (if (null? listlist)
-      e2
-      (mlet (car (car listlist))
-            (cdr (car listlist))
-            (mlet* (cdr listlist) e2))))
+;;sequentially binds each (string . MUPL Expression) pair and adds
+;; them to the same environment each time. Then evaluates muplExpression parameter
+;; in this created environment.  
+(define (mlet* racketPairList muplExpresion)
 
-(define (ifeq e1 e2 e3 e4)
-  (mlet* (list (cons "_x" e1) (cons "_y" e2))
+  ;;If racketPairList isn't null, make muplexpression using
+  (if (null? racketPairList)
+      muplExpresion
+      (mlet (car (car racketPairList))
+            (cdr (car racketPairList))
+            (mlet* (cdr racketPairList) muplExpresion))))
+
+;;Compares integers int1 and int2. If they are equal, evaluate trueResult
+;;, else evaluate falseResult
+(define (ifeq int1 int2 trueResult falseResult)
+  (mlet* (list (cons "_x" int1) (cons "_y" int2))
          (ifgreater (var "_x")
                     (var "_y")
-                    e4
+                    falseResult
                     (ifgreater (var "_y")
                                (var "_x")
-                               e4
-                               e3))))
+                               falseResult
+                               trueResult))))
 
 ;; Part 4 - Using the language
 
+;;Takes in a function and optputs a new function which then takes in a mupl list
+;; and applies the parameter function to each element in the list, then untimately
+;; returns a new mupl list with the altered values.
 (define mupl-map
   (fun #f "wrapper"
        (fun "iterate" "list"
@@ -151,6 +227,8 @@
                         (call (var "wrapper") (fst (var "list")))
                         (call (var "iterate") (snd (var "list"))))))))
 
+;;Uses the mupl-map function to make a map which adds N to each element
+;;of an integer list.
 (define mupl-mapAddN 
   (mlet "map" mupl-map
         (fun "#f" "i"
